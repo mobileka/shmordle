0ac92ca1af0610cfcaba342f862e4a37ac00a74f
Armen Markossyan <hello@armen.im>
2026-05-11 22:29:46 +0200

    Fix a bug in the evaluation (#13)

--- src/domain/evaluation.ts
+++ src/domain/evaluation.ts
@@ -20,14 +20,14 @@ export function evaluateGuess(guess: string, hidden: string, size: number = 5): 
   for (let i = 0; i < size; i++) {
     if (guess[i] === hidden[i]) {
       results[i].status = 'correct' as LetterStatus;
-      hiddenChars.splice(i, 1);
+      hiddenChars[i] = '';
     }
   }
 
   for (let i = 0; i < size; i++) {
     if (results[i].status === 'correct') continue;
 
-    let index = hiddenChars.indexOf(guess[i])
+    let index = hiddenChars.indexOf(guess[i]);
     if (index !== -1) {
       results[i].status = 'present' as LetterStatus;
 
